a2117eae7c6d3ebc7433c5396afe28c7b2792980
Aaditya Raj Uprety <aadityaru6407@gmail.com>
2026-04-30 12:28:50 +0545

    Added -ve number support, suggested by Mandip Sapkota

--- code.py
+++ code.py
@@ -1,3 +1,19 @@
+## EXCEPTIONS 
+class SymbolError(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+class SemicolonError(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+
+allowed_identifier_names = set("qwertyuiopasdfghjklzxcvbnm_1234567890")
+numbers = set("1234567890")
+operators = set("+-=*/,")
+for i in ["==","<=",">=","!=","<",">"]:
+    operators.add(i)
+
+
 
 identifiers = dict()
 def assignment(a, b):
@@ -39,6 +55,20 @@ def handle_declaration(name,dtype):
     identifiers[name]=[dtype,0]
 
 
+def check_type(ch):
+    if ch in numbers:
+        return "number"
+    if ch in allowed_identifier_names.difference(numbers):
+        return "identifier"
+    if ch in operators:
+        return "operator"
+    if ch == ";":
+        return "semicolon"
+    if ch == ",":
+        return "comma"
+
+    return "none"
+
 operators = {
     "=":[0,"assignment",assignment],
     "+":[-1,"arithmetic",add],
@@ -52,7 +82,7 @@ keywords = {
     "if":["conditional"],
 }
 decleratives_list = {"int","float"}
-
+skipset = set(" \n")
 # for faster access
 operator_set = set(operators.keys())
 
@@ -68,40 +98,56 @@ operator_set = set(operators.keys())
 # ideally line to line evaluate garna laune
 
 def lexer(exp):
-    allowed_identifier_names = set("qwertyuiopasdfghjklzxcvbnm_1234567890")
-    symbols = set("+-=*/,")
-    
-    exp_removed_newline=""
-    
-    for i in exp:
-        if i == "\n":
+    token=["none",""]
+    tokens =[]
+    listoftokens =[]
+    lineno = 0
+    charno = 0
+    for i in range(len(exp)-1):
+        charno +=1
+        curr= exp[i]
+        peek = exp[i+1]
+        if curr in skipset:
+            if token[0]!="none":
+                tokens.append(token[1])
+                token[0]="none"
+                token[1]=""
+            
+            if curr=="\n":
+                lineno+=1
+                if tokens != []:
+                    listoftokens.append(tokens)
+                    tokens =[]
+                
             continue
-        exp_removed_newline+=i
-    lis = exp_removed_newline.split(";")[:-1]
-    word_list=[]
-    line_builder=[]
-    word_builder=""
-    for line in lis:
-        for char in line:
-            if char in allowed_identifier_names:
-                word_builder += char
-            if char == " ":
-                if word_builder != "":
-                    line_builder.append(tofloat_or_int(word_builder))
-                word_builder = ""
-            if char in symbols: # BUG HERE word_list ma '' append hunxa tyo hatauna paryo
-                if word_builder != "":
-                    line_builder.append(tofloat_or_int(word_builder))
-                line_builder.append(char)
-                
-                word_builder=""
-                
-        line_builder.append(tofloat_or_int(word_builder))
-        word_list.append(line_builder)
-        word_builder =""
-        line_builder=[]
-    
-    return word_list
+        
+        curr_type = check_type(curr)
+        peek_type = check_type(peek)
+        if token[0] == "none":
+            
+            token[0] = curr_type
+        
+        token[1]+=curr
+        #print(token,curr,"'",peek,"'",curr_type,peek_type)
+        if curr_type == "number" and peek_type == "identifier":
+            raise SymbolError(f"Invalid symbol name. Identifier cannot begin with a number line = {lineno}")
+        
+        if peek_type != token[0]:
+            if peek_type != "number" or token[0]!= "identifier":
+                tokens.append(tofloat_or_int(token[1]))
+                token[0]="none"
+                token[1]=""
+                continue
+    return listoftokens 
+
+
+###
+#[['int', 'aadi', '=', 1], ['int', 'gadi', '=', 2, '+', 1, '-', 13],
+#['gadi', '=', 'aadi', '+', 5, '+', 1], ['gadi', '=', 'gadi', '+', 1], ['aadi', '=', 'aadi', '+', 'aadi']]
+#   esto khako list of lexems produce garxa lexer le ani tyo parser ma feed hunxa
+#
+#
+#
 
 
 def parser(exp):
@@ -111,13 +157,19 @@ def parser(exp):
     # if the 1st word in a line is not identified to be a keyword, we assume it is a variable
     # that is why the thing which differentiates these two is called a differentiator
     differentator = exp[0]
+    print(exp[:-1])
     if differentator in keywords:
         if keywords[differentator][0]=="declarative":
-            return parse_declarative(exp)
+            if exp[-1]!=";":
+                raise SemicolonError("Error no semicolon")
+            return parse_declarative(exp[:-1])
         # handle keyword and shii here
     else:
         # as differentiator wasnt in keywords, we assume the expression to be arithmetic
-        return parse_tree(exp)
+        
+        if exp[-1]!=";":
+            raise SemicolonError("Error no semicolon")
+        return parse_tree(exp[:-1])
         
         
 def parse_declarative(exp):
@@ -153,7 +205,9 @@ def parse_declarative(exp):
     
         
 def parse_tree(exp):
-    if len(exp)==1:
+    if len(exp) == 0:
+        return exp
+    if len(exp)==1 and exp[0] not in operator_set :
         return exp[0]
         
     result = []
@@ -168,7 +222,13 @@ def parse_tree(exp):
     # left operator right 
     # [operator,left, right]
     # we proceed L2R cuz why not
-    for i in exp:
+    #             ^   
+    #             |
+    #             |
+    # this is what i thought but 3/3*2 garda 3/3 evaluate suru ma garnu parne ani 1*3 garnu parne but suru ma 3*2 garera 3/6 garxa
+    # which is wrong so aaba R2L janxa [::-1] garera reverse gareko lol
+    
+    for i in exp[::-1]:
         if i in operators.keys():
             # Highest presidense bhettaune sath dang dung
             if (operators[i][0]==highest_presidense):
@@ -176,6 +236,11 @@ def parse_tree(exp):
                 result.append(exp[index])
                 L = parse_tree(exp[:index])
                 R = parse_tree(exp[index+1:])
+                if i in ["+","-"]:
+                    if L == []:
+                        L=0
+                    if R == []:
+                        R=0
                 result.append(L)
                 result.append(R)
 
@@ -192,14 +257,14 @@ def evaluater(exp):
         return declerative_evaluater(exp[1])
 
 def declerative_evaluater(tree):
-
+    
     for exp in tree:    
         if exp[0] == "declerative":
-            handle_declaration(exp[1][1],exp[1][0])
+            final_result = handle_declaration(exp[1][1],exp[1][0])
         if exp[0] == "arithmetic":
             parsed = parse_tree(exp[1])
-            return arithmetic_evaluater(parsed[1])
-            
+            final_result  = arithmetic_evaluater(parsed[1])
+    return final_result
 
 def arithmetic_evaluater(tree):
     
@@ -233,7 +298,7 @@ def arithmetic_evaluater(tree):
                         
                 left_side_operand = identifiers[tree[1]][1] 
             else:
-                print("WHAT THE FUCK EVEN IS ", tree[1],"?")
+                print("WHAT THE FUCK EVEN IS ", tree[1], "?")
         else:
             left_side_operand = tree[1]
     # if the operation is assignment, LHS wont need resolution
@@ -253,20 +318,24 @@ def arithmetic_evaluater(tree):
 
     return result
     
-    # return operators[ tree[0] ][1]( int(tree[1]), int(tree[2]) )  
+    # return operators[ tree[0] ][1]( int(tree[1]), int(tree[2]), ['=', 'aadi', ['arithmetic', ['+', 'aadi', 'a']]]]
+#{'a': ['int', 0 )  
     #                              ^
     # Imagine trying to understand | after a month or so
 expression = """
-int gadi = 2+1-13;
-gadi = gadi +1;
+int c=  5+ + - -5;
+
 """
 list_of_lexered = lexer(expression)
-
+print("List of lexered: ",list_of_lexered)
 
 for lexemes in list_of_lexered:
     
     parsed = parser(lexemes)
+    print(parsed)
     evaluated = evaluater(parsed)
+    print(evaluated)
 
     
+
 print(identifiers)
